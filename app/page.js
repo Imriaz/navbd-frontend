e6b5f654e04f266bd9c1bea3faf4de82432c5fc1
Tasnuba Sraboni <tasnuba.sraboni@bjitgroup.com>
2023-06-15 10:57:02 +0600

    Implementation details:  - Sass folder structure

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import logo from "../public/icon/logo.png";
+import "../styles/style.scss";
 export default function Home() {
   return (
     <div>
       <Image src={logo} alt="Logo" height={80} width={180} />
       <h1>Homepage</h1>
+      <button className="action-button">Get Started</button>
     </div>
   );
 }
