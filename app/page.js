adc7e6b94ccbf48d1818965ea8fb209c13dd40cf
Tasnuba Sraboni <tasnuba.sraboni@bjitgroup.com>
2023-07-04 10:02:58 +0600

    Create Pages

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import logo from "../public/icon/logo.png";
-import "../styles/style.scss";
 import GetStartedButton from "@/components/buttons/getStartedButton";
-import MiddleGrid from "@/components/grid/middleGrid";
 
 export default function Home() {
   return (
@@ -10,7 +8,6 @@ export default function Home() {
       <Image src={logo} alt="Logo" height={80} width={180} />
       <GetStartedButton />
       <GetStartedButton isDarkMode />
-      <MiddleGrid />
     </div>
   );
 }
