ea9eaa187f3eb9792dbf0a2bbadd93642644c4f5
Tasnuba Sraboni <tasnuba.sraboni@bjitgroup.com>
2023-07-05 09:29:12 +0600

    Create json files and image-url file

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import logo from "../public/icon/logo.png";
+import logo from "../public/image/icon/logo.svg";
+import imageurl from "@/utils/image-url";
 import GetStartedButton from "@/components/buttons/getStartedButton";
 
 export default function Home() {
   return (
     <div>
       <Image src={logo} alt="Logo" height={80} width={180} />
+      <Image src={imageurl.logo} alt="Logo" height={80} width={180} />
       <GetStartedButton />
       <GetStartedButton isDarkMode />
     </div>
